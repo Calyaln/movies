396a53bacd1a84e86f4950afd29cb25461a0540a
Calypso Asline <aslinecalypso@gmail.com>
2021-04-06 18:28:15 +0200

    sorting + pbm with path on TableHeader

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import Movies from "./components/Movies";
 
 ReactDOM.render(
   <React.StrictMode>
